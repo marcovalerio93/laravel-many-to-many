6af1a03d02dd2fa75b4d9649e0fde23e5ad1dcc8
Marco Valerio <mo.valerio93@gmal.com>
2022-10-20 15:19:39 +0200

    show tag fixed

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    public function pots(){
+    public function posts(){
         
         return $this->belongsToMany('App\Post');
         
